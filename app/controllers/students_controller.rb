ec8adee5507301084d0f90b70e58d5baa8adab52
Anna Carolina Alvarenga <anna.carolina.a92@gmail.com>
2017-09-17 12:45:23 -0300

    Adiciona e configura a gem factory_girl_rails

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,12 +1,5 @@
 class StudentsController < ApplicationController
   def index
-    if Student.count.zero?
-      Student.create!(name: 'Anna', email: 'anna.alvarenga.a92@gmail.com',
-        phone: '(11) 11111-1111', street: 'Rua Sem Nome', street_number: 1234,
-        neighborhood: 'Bairro Sem Nome', city: 'Mogi das Cruzes', state: 'SP',
-        postal_code: '08790-500')
-    end
-
     @students = Student.all
   end
 end
---
+++ spec/factories/students.rb
@@ -0,0 +1,13 @@
+FactoryGirl.define do
+  factory :student do
+    name 'Anna'
+    email 'anna.alvarenga.a92@gmail.com'
+    phone '(11) 11111-1111'
+    street 'Rua Sem Nome'
+    street_number 1234
+    neighborhood 'Bairro Sem Nome'
+    city 'Mogi das Cruzes'
+    state 'SP'
+    postal_code '08790-500'
+  end
+end
