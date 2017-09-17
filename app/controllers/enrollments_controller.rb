0f197ff8c5e6fc84b3e4617f265dcdcc015fb52b
Anna Carolina Alvarenga <anna.carolina.a92@gmail.com>
2017-09-17 20:56:06 -0300

    Implementa enrollments#create

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -6,4 +6,20 @@ class EnrollmentsController < ApplicationController
   def new
     @enrollment = Enrollment.new
   end
+
+  def create
+    @enrollment = Enrollment.new(enrollment_params)
+
+    if @enrollment.save
+      redirect_to enrollments_path, notice: 'Nova matrícula efetuada com sucesso.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def enrollment_params
+    params.require(:enrollment).permit(:student_id, :course_id, :enrolled_at)
+  end
 end
--- spec/controllers/enrollments_controller_spec.rb
+++ spec/controllers/enrollments_controller_spec.rb
@@ -16,4 +16,72 @@ RSpec.describe EnrollmentsController do
       expect(response).to be_success
     end
   end
+
+  describe 'POST #create' do
+    context 'when the params are valid' do
+      it 'does respond with 302 Found' do
+        student = create(:student)
+        course = create(:course)
+        enrollment_params = {
+          'student_id' => student.id,
+          'course_id' => course.id,
+          'enrolled_at(3i)' => '31',
+          'enrolled_at(2i)' => '01',
+          'enrolled_at(1i)' => '2017'
+        }
+
+        post :create, params: { enrollment: enrollment_params }
+
+        expect(response).to have_http_status(302)
+      end
+
+      it 'does create a new enrollment' do
+        student = create(:student)
+        course = create(:course)
+        enrollment_params = {
+          'student_id' => student.id,
+          'course_id' => course.id,
+          'enrolled_at(3i)' => '31',
+          'enrolled_at(2i)' => '01',
+          'enrolled_at(1i)' => '2017'
+        }
+
+        expect do
+          post :create, params: { enrollment: enrollment_params }
+        end.to change { Enrollment.count }.by(1)
+      end
+    end
+
+    context 'when the params are invalid' do
+      it 'does respond with 200 OK' do
+        course = create(:course)
+        enrollment_params = {
+          'student_id' => '',
+          'course_id' => course.id,
+          'enrolled_at(3i)' => '31',
+          'enrolled_at(2i)' => '01',
+          'enrolled_at(1i)' => '2017'
+        }
+
+        post :create, params: { enrollment: enrollment_params }
+
+        expect(response).to have_http_status(200)
+      end
+
+      it 'does not create a new enrollment' do
+        course = create(:course)
+        enrollment_params = {
+          'student_id' => '',
+          'course_id' => course.id,
+          'enrolled_at(3i)' => '31',
+          'enrolled_at(2i)' => '01',
+          'enrolled_at(1i)' => '2017'
+        }
+
+        expect do
+          post :create, params: { enrollment: enrollment_params }
+        end.not_to change { Enrollment.count }
+      end
+    end
+  end
 end
